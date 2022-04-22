231d5a955c13c36500410a396b002248e1a9f187
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-04-22 17:10:27 +0300

    Add project files.

---
+++ CAQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //eleman ekleme ve çıkarma işlemlerinde boxing-unboxing yaşanır.
+        Queue dynamicQueue = new Queue();
+
+        //eleman ekleme ve çıkarma sırasında boxing-unboxing yaşanmaz.
+        Queue<string> tickets = new Queue<string>();
+
+        tickets.Enqueue("Kerem Çökmez");
+        tickets.Enqueue("Çağdaş Çökmez");
+         
+        tickets.Clear();
+
+        string dequeuedTicket = tickets.Dequeue();
+        Console.WriteLine($"Dequeued Ticket : {dequeuedTicket}");
+
+        string firstTicket = tickets.Peek();
+        bool isTicketExist = tickets.Contains("Çağdaş Çökmez");
+
+        var ticketArray = tickets.ToArray();
+
+
+        Console.ReadLine();
+    }
+}
