1c95d7ab8445eb35f7bbc82f52ac751440886a87
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-04-25 08:43:59 +0300

    Added manual enqueue and dequeue methods

--- CAQueue/Program.cs
+++ CAQueue/Program.cs
@@ -2,6 +2,11 @@
 
 class Program
 {
+    private const int Dimension = 10;
+    private int front = -1;
+    private int rear = -1;
+    private int[] QueueArray = new int[Dimension];
+
     public static void Main(string[] args)
     {
         //eleman ekleme ve çıkarma işlemlerinde boxing-unboxing yaşanır.
@@ -12,7 +17,7 @@ class Program
 
         tickets.Enqueue("Kerem Çökmez");
         tickets.Enqueue("Çağdaş Çökmez");
-         
+
         tickets.Clear();
 
         string dequeuedTicket = tickets.Dequeue();
@@ -25,5 +30,75 @@ class Program
 
 
         Console.ReadLine();
+    }
+
+    void Enqueue(int number)
+    {
+        if (rear == Dimension - 1)
+        {
+            Console.WriteLine("Queue is full.");
+            return;
+        }
+        if (front == -1)
+            front = 0;
+
+        rear++;
+        QueueArray[rear] = number;
+    }
+
+    void CircularEnqueue(int number)
+    {
+        if ((front == 0 && rear == Dimension - 1) || rear + 1 == front)
+        {
+            Console.WriteLine("Queue is full.");
+            return;
+        }
+        if (front == -1)
+            front = 0;
+
+        rear = (rear + 1) % Dimension; 
+        QueueArray[rear] = number;
+    }
+
+    int Dequeue()
+    {
+        if (front == -1 || front > rear)
+        {
+            Console.WriteLine("Queue is empty.");
+        }
+
+        int dequeued = QueueArray[front];
+
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+            return dequeued;
+        }
+
+        front++;
+        return dequeued;
+    }
+
+
+    int CircularDequeue()
+    {
+        if (front == -1)
+        {
+            Console.WriteLine("Queue is empty.");
+            return -1;
+        }
+
+        int dequeued = QueueArray[front];
+
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+            return dequeued;
+        }
+
+        front = (front + 1) % Dimension;
+        return dequeued;
     }
 }
